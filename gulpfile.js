986e5208620fea65d5c70d6b7ebb0725e6625919
Alexey Batin <batin.alexey@gmail.com>
2018-05-23 21:10:18 +0300

    Add new tasks

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('browser-sync', function() {
 		// open: false,
 		// online: false, // Work Offline Without Internet Connection
 		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
-	})
+	});
 });
 
 gulp.task('styles', function() {
@@ -31,7 +31,7 @@ gulp.task('styles', function() {
 	.pipe(autoprefixer(['last 15 versions']))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
 	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream())
+	.pipe(browserSync.stream());
 });
 
 gulp.task('js', function() {
@@ -42,13 +42,44 @@ gulp.task('js', function() {
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
 	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({ stream: true }))
+	.pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
 	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
-	gulp.watch('app/*.html', browserSync.reload)
+	gulp.watch('app/*.html', browserSync.reload);
 });
+
+gulp.task('img', function () {
+  return gulp.src('app/img/**/*')
+    .pipe(cache(imagemin({
+      interlaced: true,
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    })))
+    .pipe(gulp.dest('dist/img'));
+});
+
+
+gulp.task('build', ['img', 'styles', 'js'], function () {
+
+  var buildCss = gulp.src('app/css/main.min.css')
+    .pipe(gulp.dest('dist/css'));
+
+  var buildFonts = gulp.src('app/webfonts/*')
+    .pipe(gulp.dest('dist/webfonts/'));
+
+  var buildJs = gulp.src('app/js/scripts.min.js')
+    .pipe(gulp.dest('dist/js'));
+
+  var buildHtml = gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+
+});
+
 
 gulp.task('default', ['watch']);
